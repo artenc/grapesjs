3d8d0df641545bcacfb85b5d07fdbd4739be97cd
artenc <arten.c.g@gmail.com>
2024-03-18 13:25:15 +0200

    Add ability to use own undo manager instance
    (cherry picked from commit 7edf497b59efd6bcddf66e9108924346601e6d11)

--- src/undo_manager/config.ts
+++ src/undo_manager/config.ts
@@ -9,11 +9,17 @@ export interface UndoManagerConfig {
    * @default true
    */
   trackSelection?: boolean;
+
+  /**
+   * Use the property to use own instance of undo manager
+   */
+  undoManagerInstance?: Object | Function;
 }
 
 const config: UndoManagerConfig = {
   maximumStackLength: 500,
   trackSelection: true,
+  undoManagerInstance: undefined,
 };
 
 export default config;
